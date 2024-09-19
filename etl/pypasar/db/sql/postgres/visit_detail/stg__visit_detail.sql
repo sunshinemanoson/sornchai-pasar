dd966e7bafbac9a12032e4547f3d2262f8aee244
sunshinemanoson <manosonsunshine@gmail.com>
2024-09-19 06:09:27 +0000

    modified stg__visit_detail.sql

--- etl/pypasar/db/sql/postgres/visit_detail/stg__visit_detail.sql
+++ etl/pypasar/db/sql/postgres/visit_detail/stg__visit_detail.sql
@@ -2,18 +2,18 @@
 CREATE OR REPLACE VIEW {OMOP_SCHEMA}.stg__visit_detail AS
     -- Extract relevant columns from the pre_op.char table
     WITH source AS (
-        SELECT 
+        SELECT
             po.id AS po_id, 
-            pre.id AS pre_id,  
-            po.session_startdate,
+            po.session_startdate AS po_session_startdate,
             po.anon_case_no,
-            omp.person_source_value AS person_id,
-            pre.Admission_Type,
             po.session_id,
+            omp.person_source_value,
+            omp.person_id,
+            vio.visit_source_value,
             opr.session_id AS opr_session_id,
             opr.anon_surgeon_name AS surgeon_name,
-            opr.anon_plan_anaesthetist_1_name AS anaesthetist_1_name,
-            opr.anon_plan_anaesthetist_2_name AS anaesthetist_2_name,
+            -- opr.anon_plan_anaesthetist_1_name AS anaesthetist_1_name,
+            -- opr.anon_plan_anaesthetist_2_name AS anaesthetist_2_name,
             po.icu_admission_date,
             po.icu_admission_time,
             po.operation_starttime,
@@ -21,33 +21,36 @@ CREATE OR REPLACE VIEW {OMOP_SCHEMA}.stg__visit_detail AS
             po.icu_discharge_date,
             po.icu_discharge_time,
             po.icu_location
-        FROM {POSTOP_SCHEMA}.icu po
-        LEFT JOIN {PREOP_SCHEMA}.char pre 
-            ON pre.anon_case_no = po.anon_case_no
-        LEFT JOIN {OMOP_SCHEMA}.person omp 
+        FROM postop.icu po
+        LEFT JOIN omop_sqldev_schema.person omp 
             ON omp.person_source_value = po.anon_case_no
-        LEFT JOIN {INTRAOP_SCHEMA}.operation opr
-            ON pre.anon_case_no = opr.anon_case_no 
+        LEFT JOIN omop_sqldev_schema.visit_occurrence vio 
+            ON po.anon_case_no = vio.visit_source_value 
+		LEFT JOIN intraop.operation opr
+            ON po.anon_case_no = opr.anon_case_no 
+        LIMIT 1000
     ),
     filteredSource AS (
         SELECT 
             *,
-            ROW_NUMBER() OVER (PARTITION BY person_id ORDER BY session_startdate) AS unique_person_id,
-            ROW_NUMBER() OVER (PARTITION BY session_id,  anon_case_no ORDER BY session_startdate) AS unique_visit_occurrence_id,
-            ROW_NUMBER() OVER (ORDER BY session_startdate, po_id) AS  unique_visit_detail_id,
-            ROW_NUMBER() OVER (PARTITION BY opr_session_id, surgeon_name,anaesthetist_1_name,anaesthetist_2_name  ORDER BY session_startdate) AS unique_provider_id
+            -- ROW_NUMBER() OVER (PARTITION BY session_id,  anon_case_no ORDER BY session_startdate) AS unique_visit_occurrence_id,
+            ROW_NUMBER() OVER ( ORDER BY session_id , po_session_startdate) AS  unique_visit_detail_id,
+			NULL AS unique_visit_occurrence_id,
+            NULL AS unique_provider_id
+            -- ROW_NUMBER() OVER (PARTITION BY opr_session_id, surgeon_name,anaesthetist_1_name,anaesthetist_2_name  ORDER BY session_startdate) AS unique_provider_id
         FROM source s
     ),
     mapping AS (
         SELECT
             unique_visit_detail_id,
-            unique_person_id AS person_id,
-            CASE 
-                WHEN Admission_Type = 'Inpatient' THEN 9201 
-                WHEN Admission_Type = 'Day Surgery (DS)' THEN 9202 
-                WHEN Admission_Type = 'Same Day Admission (SDA)' THEN 9203
-                ELSE NULL
-            END AS visit_detail_concept_id,
+            person_id,
+            -- CASE 
+            --     WHEN Admission_Type = 'Inpatient' THEN 9201 
+            --     WHEN Admission_Type = 'Day Surgery (DS)' THEN 9202 
+            --     WHEN Admission_Type = 'Same Day Admission (SDA)' THEN 9203
+            --     ELSE NULL
+            -- END AS visit_detail_concept_id,
+            32037 AS visit_detail_concept_id,
             -- if icu_admission_date,icu_discharge_date is null values is 2000-01-01
             COALESCE(icu_admission_date, '2000-01-01') AS check_icu_admission_date,
             COALESCE((icu_admission_date + operation_starttime::interval), '2000-01-01') AS concat_start_datetime,
@@ -73,7 +76,7 @@ CREATE OR REPLACE VIEW {OMOP_SCHEMA}.stg__visit_detail AS
         FROM filteredSource fs
         JOIN mapping m
             ON fs.unique_visit_detail_id = m.unique_visit_detail_id 
-        ORDER BY fs.session_startdate
+        ORDER BY visit_detail_id
     )
     SELECT
         visit_detail_id,
@@ -86,4 +89,5 @@ CREATE OR REPLACE VIEW {OMOP_SCHEMA}.stg__visit_detail AS
         visit_detail_end_datetime,
         provider_id,
         visit_occurrence_id
-    FROM   final;
+    FROM   final
+	;
